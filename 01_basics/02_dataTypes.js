cbf4cd330be6f72b530c28d2bfee2b91515f0fed
Dushyant <singhdushyant2989@gmail.com>
2023-07-27 18:00:32 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer versions
+
+// alert("hello") // we are using nodejs, not browser
+
+console.log(3 + 3);
+
+console.log("Dushyant");
+
+let name = "Dushyant";
+let age = 23;
+let isLoggedIn = false;
+
+// number => 2 to power 53 range
+// bigInt =>
+// string => ""
+// boolean => true or false
+// null => stand alone value it is object type
+// undefined => value is not assigned to variable
+// symbol => unique
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
