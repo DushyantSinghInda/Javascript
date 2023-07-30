2b3cefda53798e41093971b9f80c9b8343c9d633
Dushyant <singhdushyant2989@gmail.com>
2023-07-30 17:23:25 +0530

    Datatype summary

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
-let score = "dushyant";
+// let score = "dushyant";
 
-console.log(typeof score);
-console.log(typeof score);
+// console.log(typeof score);
+// console.log(typeof score);
 
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN Not a Number
@@ -13,19 +13,45 @@ console.log(valueInNumber);
 // null => 0
 // undefined => NaN
 
-let isLoggedIn = "dushyant";
+// let isLoggedIn = "dushyant";
 
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
 // "dushyant" => true
 
-let someNumber = 33;
+// let someNumber = 33;
 
-let stringNumber = String(someNumber);
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// ******************** Operations *********************
+
+let value = 3;
+let negvalue = -value;
+// console.log(negvalue);
+
+let str1 = "Hello";
+let str2 = " Dushyant";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+// ++gameCounter;
+console.log(++gameCounter);
---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,35 @@
+// JavaScript is dynamic type language
+// # Primitive
+
+//  7types : String, Number, Boolean, null, undefined, Symbol, bigInt
+
+const score = false;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol("123");
+const anotherId = Symbol("123");
+
+// console.log(id);
+// console.log(anotherId);
+
+const bigNumber = 32563565476767853754657856n; //for bigInt
+
+// Reference (Non-primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+  name: "Dushyant",
+  age: 23,
+};
+
+const myFunction = function () {
+  console.log("Hello world");
+};
+
+console.log(typeof anotherId);
