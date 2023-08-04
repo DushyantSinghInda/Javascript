e424b51942bed8e9de45ddae640eb77da4523083
Dushyant <singhdushyant2989@gmail.com>
2023-08-04 17:17:37 +0530

    Control flow completed

---
+++ 04_control_flow/one.js
@@ -0,0 +1,35 @@
+//  if
+
+const balance = 1000;
+
+// if (balance >= 500) console.log("test"), console.log("test2");  //bad practice
+
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march";
+
+switch (month) {
+  case "jan":
+    console.log("January");
+    break;
+  case "feb":
+    console.log("Feb");
+    break;
+  case "march":
+    console.log("March");
+    break;
+  case "april":
+    console.log("April");
+    break;
+
+  default:
+    console.log("Default case match");
+    break;
+}
