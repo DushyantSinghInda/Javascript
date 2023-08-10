f5212f290da09956265d5031e09165f1d3d9980e
Dushyant <singhdushyant2989@gmail.com>
2023-08-10 17:44:20 +0530

    for some time its final push

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -50,10 +50,10 @@ function handleObject(anyObject) {
 }
 
 // handleObject(user);
-handleObject({
-  username: "sam",
-  price: 399,
-});
+// handleObject({
+//   username: "sam",
+//   price: 399,
+// });
 
 const myNewArray = [200, 400, 100, 600];
 
@@ -62,4 +62,31 @@ function returnSecondVaue(getArray) {
 }
 
 // console.log(returnSecondVaue(myNewArray));
-console.log(returnSecondVaue([200, 400, 100, 600]));
+// console.log(returnSecondVaue([200, 400, 100, 600]));
+
+function inner() {
+  return "Reference or function call";
+}
+
+const re = inner; // this 'inner' provide reference of the inner function to the re variable
+const call = inner(); // this 'inner()' here calls the inner function
+
+// console.log(re);
+// console.log(call);
+
+// Callback function
+
+function add(a, b) {
+  return a + b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function calculate(x, y, operation) {
+  return operation(x, y);
+}
+
+console.log(calculate(2, 2, add));
+console.log(calculate(2, 2, divide));
